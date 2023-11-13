049fb64d86d9a4a57e9d3c3e4cf836c37ea19faa
Wunkolo <Wunkolo@gmail.com>
2023-11-13 15:24:45 -0800

    vk_instance: Detect MSAA features
    The MSAA _requires_ `sampleRateShading`, and also requires
    `VK_KHR_create_renderpass2` and `VK_KHR_depth_stencil_resolve` for the
    depth-stencil MSAA resolves.

--- src/video_core/renderer_vulkan/vk_instance.h
+++ src/video_core/renderer_vulkan/vk_instance.h
@@ -248,6 +248,11 @@ public:
         return triangle_fan_supported;
     }
 
+    // Returns true when sampleRateShading, VK_KHR_create_renderpass2, VK_KHR_depth_stencil_resolve
+    bool IsMSAASupported() const {
+        return features.sampleRateShading && create_renderpass2 && depth_stencil_resolve;
+    }
+
     /// Returns the minimum vertex stride alignment
     u32 GetMinVertexStrideAlignment() const {
         return min_vertex_stride_alignment;
@@ -318,6 +323,8 @@ private:
     bool index_type_uint8{};
     bool fragment_shader_interlock{};
     bool image_format_list{};
+    bool create_renderpass2{};
+    bool depth_stencil_resolve{};
     bool pipeline_creation_cache_control{};
     bool fragment_shader_barycentric{};
     bool shader_stencil_export{};
