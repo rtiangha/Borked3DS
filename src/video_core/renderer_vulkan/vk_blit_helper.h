e99aa4a08776a70819c833c2b8d6ef1464352e9b
Wunkolo <Wunkolo@gmail.com>
2023-11-13 13:05:31 -0800

    vk_blit_helper: Add d24s8->rgba8 multi-sample blit

--- src/video_core/renderer_vulkan/vk_blit_helper.h
+++ src/video_core/renderer_vulkan/vk_blit_helper.h
@@ -55,10 +55,12 @@ private:
 
     vk::ShaderModule full_screen_vert;
     vk::ShaderModule d24s8_to_rgba8_comp;
+    vk::ShaderModule d24s8_to_rgba8_ms_comp;
     vk::ShaderModule depth_to_buffer_comp;
     vk::ShaderModule blit_depth_stencil_frag;
 
     vk::Pipeline d24s8_to_rgba8_pipeline;
+    vk::Pipeline d24s8_to_rgba8_ms_pipeline;
     vk::Pipeline depth_to_buffer_pipeline;
     vk::Pipeline depth_blit_pipeline;
     vk::Sampler linear_sampler;
