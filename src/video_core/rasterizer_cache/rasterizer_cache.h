ed67ca54090c399536a3ef93528967c59c7feb79
Wunkolo <Wunkolo@gmail.com>
2023-11-20 09:12:30 -0800

    rasterizer_cache: Remove MSAA from fill-surfaces and display-transfers

--- src/video_core/rasterizer_cache/rasterizer_cache.h
+++ src/video_core/rasterizer_cache/rasterizer_cache.h
@@ -274,7 +274,6 @@ bool RasterizerCache<T>::AccelerateDisplayTransfer(const Pica::DisplayTransferCo
     src_params.height = config.output_height;
     src_params.is_tiled = !config.input_linear;
     src_params.pixel_format = PixelFormatFromGPUPixelFormat(config.input_format);
-    src_params.sample_count = sample_count;
     src_params.UpdateParams();
 
     SurfaceParams dst_params;
@@ -346,7 +345,6 @@ bool RasterizerCache<T>::AccelerateFill(const Pica::MemoryFillConfig& config) {
     params.size = params.end - params.addr;
     params.type = SurfaceType::Fill;
     params.res_scale = std::numeric_limits<u16>::max();
-    params.sample_count = sample_count;
 
     SurfaceId fill_surface_id = slot_surfaces.insert(runtime, params);
     Surface& fill_surface = slot_surfaces[fill_surface_id];
