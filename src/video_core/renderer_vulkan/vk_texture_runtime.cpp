6bd7a1cd93e8257390dc122a5180bbe947fbcea3
Reg Tiangha <rtiangha@users.noreply.github.com>
2024-04-07 21:20:51 -0600

    apply clang-format-15

--- src/video_core/renderer_vulkan/vk_texture_runtime.cpp
+++ src/video_core/renderer_vulkan/vk_texture_runtime.cpp
@@ -1578,8 +1578,8 @@ void Surface::BlitScale(const VideoCore::TextureBlit& blit, bool up_scale) {
 
 Framebuffer::Framebuffer(TextureRuntime& runtime, const VideoCore::FramebufferParams& params,
                          Surface* color, Surface* depth)
-    : VideoCore::FramebufferParams{params},
-      res_scale{color ? color->res_scale : (depth ? depth->res_scale : 1u)},
+    : VideoCore::FramebufferParams{params}, res_scale{color ? color->res_scale
+                                                            : (depth ? depth->res_scale : 1u)},
       sample_count{params.sample_count} {
     auto& renderpass_cache = runtime.GetRenderpassCache();
     if (shadow_rendering && !color) {
@@ -1684,8 +1684,8 @@ Sampler::Sampler(TextureRuntime& runtime, const VideoCore::SamplerParams& params
 Sampler::~Sampler() = default;
 
 DebugScope::DebugScope(TextureRuntime& runtime, Common::Vec4f color, std::string_view label)
-    : scheduler{runtime.GetScheduler()},
-      has_debug_tool{runtime.GetInstance().HasDebuggingToolAttached()} {
+    : scheduler{runtime.GetScheduler()}, has_debug_tool{
+                                             runtime.GetInstance().HasDebuggingToolAttached()} {
     if (!has_debug_tool) {
         return;
     }
