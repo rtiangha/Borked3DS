96fb4bed35b7917c589b0cae105d50dc132d7dbd
Reg Tiangha <rtiangha@users.noreply.github.com>
2024-08-23 22:31:51 -0600

    Android: Update cmake to 3.30.2

--- src/android/app/build.gradle.kts
+++ src/android/app/build.gradle.kts
@@ -173,7 +173,7 @@ android {
 
     externalNativeBuild {
         cmake {
-            version = "3.22.1"
+            version = "3.30.2"
             path = file("../../../CMakeLists.txt")
         }
     }
