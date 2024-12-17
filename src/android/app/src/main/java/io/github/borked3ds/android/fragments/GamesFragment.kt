3464bff1d68ecc6892c9da6a56c61f39f726da7d
Kleidis <167202775+kleidis@users.noreply.github.com>
2024-12-17 13:49:03 +0100

    Add a games folder button

--- src/android/app/src/main/java/io/github/borked3ds/android/fragments/GamesFragment.kt
+++ src/android/app/src/main/java/io/github/borked3ds/android/fragments/GamesFragment.kt
@@ -7,6 +7,7 @@ package io.github.borked3ds.android.fragments
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import android.content.res.Configuration
 import android.os.Bundle
@@ -16,6 +17,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.ViewGroup.MarginLayoutParams
 import android.widget.PopupMenu
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -40,6 +42,7 @@ import io.github.borked3ds.android.adapters.GameAdapter
 import io.github.borked3ds.android.databinding.FragmentGamesBinding
 import io.github.borked3ds.android.features.settings.model.Settings
 import io.github.borked3ds.android.model.Game
+import io.github.borked3ds.android.utils.GameHelper
 import io.github.borked3ds.android.viewmodel.GamesViewModel
 import io.github.borked3ds.android.viewmodel.HomeViewModel
 import java.time.temporal.ChronoField
@@ -61,6 +64,25 @@ class GamesFragment : Fragment() {
     private val homeViewModel: HomeViewModel by activityViewModels()
 
     private lateinit var preferences: SharedPreferences
+
+    // TO BE REPLACED WITH MULTIPLE GAME FOLDER SUPPORT SOON
+    private val getGamesDirectory =
+        registerForActivityResult(ActivityResultContracts.OpenDocumentTree()) { result ->
+            if (result == null) {
+                return@registerForActivityResult
+            }
+
+            requireContext().contentResolver.takePersistableUriPermission(
+                result,
+                Intent.FLAG_GRANT_READ_URI_PERMISSION
+            )
+
+            preferences.edit()
+                .putString(GameHelper.KEY_GAME_PATH, result.toString())
+                .apply()
+
+            homeViewModel.setGamesDir(requireActivity(), result.path!!)
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -184,6 +206,10 @@ class GamesFragment : Fragment() {
 
         setInsets()
         setupTopView()
+
+        binding.addDirectory.setOnClickListener {
+            getGamesDirectory.launch(Intent(Intent.ACTION_OPEN_DOCUMENT_TREE).data)
+        }
     }
 
     override fun onDestroyView() {
