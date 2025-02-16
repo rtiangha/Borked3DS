b5bdbe3441ae763997eff0cc6362bdcd6e388d4a
Reg Tiangha <rtiangha@users.noreply.github.com>
2025-02-16 16:39:37 -0700

    Make verbose logging the default (#350)
    Sets the default logging level to `:Trace` rather than `:Info` and
    `instant_debug_log` to `true` to help with debugging and troubleshooting.
    Users can easily reduce the verbosity back to :Info themselves if they wish.

--- src/common/settings.h
+++ src/common/settings.h
@@ -610,7 +610,7 @@ struct Values {
     Setting<bool> delay_start_for_lle_modules{true, "delay_start_for_lle_modules"};
     Setting<bool> use_gdbstub{false, "use_gdbstub"};
     Setting<u16> gdbstub_port{24689, "gdbstub_port"};
-    Setting<bool> instant_debug_log{false, "instant_debug_log"};
+    Setting<bool> instant_debug_log{true, "instant_debug_log"};
 
     // Hacks
     SwitchableSetting<bool> enable_custom_cpu_ticks{false, "enable_custom_cpu_ticks"};
@@ -623,7 +623,7 @@ struct Values {
     SwitchableSetting<bool> upscaling_hack{false, "upscaling_hack"};
 
     // Miscellaneous
-    Setting<std::string> log_filter{"*:Info", "log_filter"};
+    Setting<std::string> log_filter{"*:Trace", "log_filter"};
     Setting<std::string> log_regex_filter{"", "log_regex_filter"};
 
     // Video Dumping
