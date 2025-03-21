32f1d2a3696356ec0e0a87643205677b860df889
OpenSauce04 <opensauce04@gmail.com>
2025-03-21 15:57:31 +0000

    video_core: Fixed emulation window artefacts on OpenGL + Wayland

--- src/video_core/renderer_opengl/renderer_opengl.cpp
+++ src/video_core/renderer_opengl/renderer_opengl.cpp
@@ -1,5 +1,6 @@
 // Copyright 2022 Citra Emulator Project
 // Copyright 2024 Borked3DS Emulator Project
+// Copyright 2025 Azahar Emulator Project
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
 
@@ -306,7 +307,7 @@ void RendererOpenGL::FillScreen(Common::Vec3<u8> color, TextureInfo& texture) {
  */
 void RendererOpenGL::InitOpenGLObjects() {
     glClearColor(Settings::values.bg_red.GetValue(), Settings::values.bg_green.GetValue(),
-                 Settings::values.bg_blue.GetValue(), 0.0f);
+                 Settings::values.bg_blue.GetValue(), 1.0f);
 
     for (std::size_t i = 0; i < samplers.size(); i++) {
         samplers[i].Create();
@@ -638,7 +639,7 @@ void RendererOpenGL::DrawScreens(const Layout::FramebufferLayout& layout, bool f
     if (settings.bg_color_update_requested.exchange(false)) {
         // Update background color before drawing
         glClearColor(Settings::values.bg_red.GetValue(), Settings::values.bg_green.GetValue(),
-                     Settings::values.bg_blue.GetValue(), 0.0f);
+                     Settings::values.bg_blue.GetValue(), 1.0f);
     }
 
     if (settings.shader_update_requested.exchange(false)) {
